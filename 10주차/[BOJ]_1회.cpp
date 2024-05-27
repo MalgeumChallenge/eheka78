ee39a7c590cd0bae8f16450ce15457f8ac4ef1b0
eheka78 <123297062+Dodam1st@users.noreply.github.com>
2024-05-28 01:06:21 +0900

    Add files via upload

---
+++ 10주차/[BOJ]_1회.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int cnt{ 0 };
+    while (s.size() > 1) { 
+        int sum{ 0 };
+        for (int i{ 0 }; i < s.length(); i++) {
+            sum += s[i] - '0'; 
+        }
+        s = to_string(sum); 
+        cnt++;
+    }
+
+    cout << cnt << "\n"; 
+
+    if (stoi(s) % 3 == 0) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
