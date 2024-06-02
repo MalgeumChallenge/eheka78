4c4ac62046e6bbd57420e12de7f278f424b915d9
eheka78 <123297062+Dodam1st@users.noreply.github.com>
2024-06-03 04:33:51 +0900

    Add files via upload

---
+++ 10주차/[BOJ]_3회.cpp
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/2156
+// 2156번
+// 포도주 시식
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int a[n + 1] = { 0, };
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    long long dp[n + 1];
+
+    dp[0] = 0;
+    dp[1] = a[1];
+    dp[2] = a[1] + a[2];
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = max({ dp[i - 3] + a[i - 1] + a[i], dp[i - 2] + a[i], dp[i - 1] });
+    }
+
+    cout << dp[n] << endl;
+}
